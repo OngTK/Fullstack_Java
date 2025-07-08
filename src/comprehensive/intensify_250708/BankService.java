0bc0268bef5ac69e0fd2adda8259dd7e5bdff6bc
OngTK <dhdxorud@gmail.com>
2025-07-08 17:38:14 +0900

    250708_1738

--- src/comprehensive/intensify_250708/BankService.java
+++ src/comprehensive/intensify_250708/BankService.java
@@ -8,24 +8,27 @@ public class BankService {
         Scanner scan = new Scanner(System.in);
         BankController ct = new BankController();
 
-        for (; ; ) {
+        for ( ; ; ) {
             System.out.println("============== KB Bank  ==============");
             System.out.println("1.계좌등록 | 2.입금 | 3.출금 | 4.잔고");
             System.out.println("======================================");
-            System.out.print("선택 >");
+            System.out.print("선택 > ");
             int choice = scan.nextInt();
             ;
 
-            if (choice == 1) { // 1. 계좌 등록
+            if (choice == 1) {                      // 1. 계좌 등록
                 System.out.println("--- 계좌 등록 ---");
                 System.out.print("계좌번호 : ");
                 String accountNo = scan.next();
                 System.out.print("비밀번호 : ");
                 int pw = scan.nextInt();
 
-                ct.createAccount(accountNo, pw);
+                boolean result = ct.createAccount(accountNo, pw);
+                if(result){
+                    System.out.println("[안내] 계좌 등록이 완료되었습니다.");
+                }
 
-            } else if (choice == 2) { // 2. 입금
+            } else if (choice == 2) {               // 2. 입금
                 System.out.println("--- 입금 ---");
                 System.out.print("계좌번호 : ");
                 String accountNo = scan.next();
@@ -34,9 +37,14 @@ public class BankService {
                 System.out.print("입금액 : ");
                 int balance = scan.nextInt();
 
-                ct.deposit(accountNo, pw, balance);
+                boolean result = ct.deposit(accountNo, pw, balance);
+                if (result) {
+                    System.out.println("[안내] 입금이 완료되었습니다.");
+                } else {
+                    System.out.println("[경고] 계좌정보가 일치하지 않습니다.");
+                }
 
-            } else if (choice == 3) { // 3. 출금
+            } else if (choice == 3) {               // 3. 출금
                 System.out.println("--- 출금 ---");
                 System.out.print("계좌번호 : ");
                 String accountNo = scan.next();
@@ -45,16 +53,28 @@ public class BankService {
                 System.out.print("출금액 : ");
                 int withdrawAmount = scan.nextInt();
 
-                ct.withdrawal(accountNo,pw,withdrawAmount);
+                int result = ct.withdrawal(accountNo,pw,withdrawAmount);
+                if (result == 1){
+                    System.out.println("[안내] 출금이 완료되었습니다.");
+                } else if (result == 2){
+                    System.out.println("[경고] 잔액이 부족합니다.");
+                } else if (result == 3){
+                    System.out.println("[경고] 계좌정보가 일치하지 않습니다.");
+                }
 
-            } else if (choice == 4) { // 4. 잔고
+            } else if (choice == 4) {                // 4. 잔고
                 System.out.println("--- 잔고 확인 ---");
                 System.out.print("계좌번호 : ");
                 String accountNo = scan.next();
                 System.out.print("비밀번호 : ");
                 int pw = scan.nextInt();
 
-                ct.viewBalance(accountNo, pw);
+                int result = ct.viewBalance(accountNo, pw);
+                if (result >= 0) {
+                    System.out.printf("[잔고] %d원\n", result);
+                } else {
+                    System.out.println("[경고] 계좌정보가 일치하지 않습니다.");
+                }
 
             } else {
                 System.out.println("[경고] 올바르지 못한 기능입니다.");
