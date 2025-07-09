c35668068cf13b5f8ea74f126847e9e88dadcc60
OngTK <dhdxorud@gmail.com>
2025-07-09 18:04:21 +0900

    250709_1804

--- src/comprehensive/intensify_02_250709/BankService.java
+++ src/comprehensive/intensify_02_250709/BankService.java
@@ -41,7 +41,12 @@ public class BankService {
                 System.out.print("입금액 : ");
                 int money = scan.nextInt();
 
-                // 입금 메소드 실행 >> return boolean
+                // 2. 입금
+                // 유효성 검사
+                // ture :
+                //      해당 계좌번호에 balance에 입금 / return true
+                // false : return false
+
                 boolean result = bc.deposit(accountNo, pw, money);
 
                 // 계좌번호, 비밀번호 유효성 검사 > ture : [안내] 입금이 완료되었습니다. / false : [경고] 계좌정보가 일치하지 않습니다.
@@ -58,6 +63,12 @@ public class BankService {
                 int pw = scan.nextInt();
                 System.out.print("출금액 : ");
                 int money = scan.nextInt();
+
+                // 3. 출금 메소드 실행 >> return int
+                // 유효성 검사
+                // ture :  ( 잔고 > 출금 )[안내] 출금이 완료되었습니다.  // return 1 : 출금완료
+                //         ( 잔고 < 출금) [경고] 잔액이 부족합니다.      // return 2 : 금액부족 출금실패
+                // false : [경고] 계좌정보가 일치하지 않습니다.          // return 3 : 유효성검사 false
 
                 int result = bc.withdraw(accountNo, pw, money);
                 if(result == 1) {
@@ -77,11 +88,10 @@ public class BankService {
                 int pw = scan.nextInt();
 
 
-                // 잔고 조회 메소드 실행 >> return balance
-                // 계좌번호, 비밀번호 유효성 검사
-                // ture :  [잔고] 00원
-                // false : [경고] 계좌정보가 일치하지 않습니다.
-
+                // 4. 잔고 조회 메소드 실행 >> return int
+                // 유효성 검사
+                // ture :  [잔고] 00원                         // return balance (balance >= 0)
+                // false : [경고] 계좌정보가 일치하지 않습니다.    // return -1
                 int result = bc.viewBalance(accountNo, pw);
                 if (result >= 0){
                     System.out.printf("[잔고] %d원 \n", result);
@@ -100,16 +110,16 @@ public class BankService {
                 System.out.print("이체 금액 : ");
                 int money = scan.nextInt();
 
-                // 계좌이체 fun >> return boolen
-                // 계좌번호, 비밀번호 유효성 검사
-                // true
-                //      받는분 계좌번호에 대한 유효성 검사
-                //          ture : [안내] 이체가 완료되었습니다.
-                //          false : [경고] 받는 분의 계좌번호를 다시 확인해주세요.
-                // false : 계좌정보가 일치하지 않습니다.
+                // 5. 계좌이체
+                // 유효성 검사
+                // ture :  if (이체금액 > 잔액)
+                //        ture : 금액부족                           // return 0
+                //        flase : 잔액이 충분 = if 이체할 계좌 검색
+                //                              ture  이체 성공      // return 1
+                //                              false 이체 실패      // return -2
+                // false - 계좌정보 불일치                             // return -1
 
                 int result = bc.transfer(accountNo,pw,receiveAccountNo,money);
-
                 if(result == 0) {
                     System.out.println("[경고] 잔액이 부족합니다.");
                 } else if(result == 1) {
