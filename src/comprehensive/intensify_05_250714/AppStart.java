e6c6b9f0c79e11b4ab99f4be4a8fd9d9014e7141
OngTK <dhdxorud@gmail.com>
2025-07-14 18:16:47 +0900

    250714_1816

--- src/comprehensive/intensify_05_250714/AppStart.java
+++ src/comprehensive/intensify_05_250714/AppStart.java
@@ -1,5 +1,7 @@
 package comprehensive.intensify_05_250714;
 
+//https://docs.google.com/spreadsheets/d/165H1LlSHPIidvmAXbxkiuvydcwR4CxDyH4cpxPYBkYk/edit?gid=373240839#gid=373240839
+
 import comprehensive.intensify_05_250714.view.BankView;
 
 // AppStart 역할 : 프로그램 시작 클래스
