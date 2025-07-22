a3e7aae78bc6f9cf71447c89829b521a73039fc3
OngTK <dhdxorud@gmail.com>
2025-07-22 12:56:52 +0900

    250722_1256

--- src/MVC_folderFormat/AppStart.java
+++ src/MVC_folderFormat/AppStart.java
@@ -4,6 +4,7 @@ package MVC_folderFormat;
 
 import comprehensive.Example_09_250722.controller.BoardController;
 import comprehensive.Example_09_250722.model.dto.BoardDto;
+import comprehensive.Example_09_250722.view.BoardView;
 
 import java.util.ArrayList;
 
@@ -11,15 +12,19 @@ public class AppStart {
     public static void main(String[] args) {
 
         // view 작업전 controller / dao test
-        // 1. 테스트할 컨트롤러 싱글톤 가져오기
-        BoardController BC = BoardController.getInstance();
+//        // 1. 테스트할 컨트롤러 싱글톤 가져오기
+//        BoardController BC = BoardController.getInstance();
+//
+//        // 2. 주석처리하면서 단위 기능 테스트
+//        // 2.1. 등록
+//        BC.boardWrite("안녕하세요.", "user");
+//
+//        // 2.2. 전체조회
+//        ArrayList<BoardDto> result = BC.boardPrint();
+//        System.out.println(result);
 
-        // 2. 주석처리하면서 단위 기능 테스트
-        // 2.1. 등록
-        BC.boardWrite("안녕하세요.", "user");
+        BoardView.getInstance().index();
 
-        // 2.2. 전체조회
-        ArrayList<BoardDto> result = BC.boardPrint();
-        System.out.println(result);
+
     } // main end
 } //class end
--- src/comprehensive/Example_09_250722/view/BoardView.java
+++ src/comprehensive/Example_09_250722/view/BoardView.java
@@ -1,7 +1,9 @@
 package comprehensive.Example_09_250722.view;
 
 import comprehensive.Example_09_250722.controller.BoardController;
+import comprehensive.Example_09_250722.model.dto.BoardDto;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 // View 역할 : 게시물 관련 입출력
@@ -15,13 +17,56 @@ public class BoardView {
     }
 
     // Controller 싱글톤 호출  =======================================
-    private BoardController Controller = BoardController.getInstance();
+    private BoardController controller = BoardController.getInstance();
 
     // 0. 여러 메소드에서 사용할 입력 객체를 입력변수로 선언 =================================
-
     private Scanner scan = new Scanner(System.in);
 
     // main view : 최초로 보이는 화면 구성 =================================
 
+    // [1] 메인
+    public void index(){
+        for(;;){
+            System.out.println("=======================");
+            System.out.println(" 1.등록 | 2. 전체조회");
+            System.out.println("=======================");
+            System.out.print("선택 > ");
+            int choice = scan.nextInt();
 
+            if (choice == 1 ){
+                boardWrite();
+            } else if (choice == 2) {
+                boardPrint();
+            }
+        } // infinity roof end
+    } // index end
+    
+    // [2] 등록
+    public void boardWrite(){
+        System.out.print("내용 : ");
+        String content = scan.next();
+        System.out.print("작성자 : ");
+        String writer = scan.next();
+
+        boolean result = controller.boardWrite(content,writer);
+
+        if(result){
+            System.out.println("[안내] 등록을 성공하였습니다.");
+        } else {
+            System.out.println("[경고] 등록을 실패하였습니다.");
+        }
+    }
+    
+    // [3] 전체조회
+    public void boardPrint(){
+        System.out.println("===================게시물 조회===================");
+
+        ArrayList<BoardDto> boardDB = controller.boardPrint();
+
+        for (BoardDto dto : boardDB) {
+            System.out.println("작성자 : " + dto.getWriter());
+            System.out.println("내용 : " + dto.getContent());
+            System.out.println("---------------------------");
+        }
+    }
 } //class end
