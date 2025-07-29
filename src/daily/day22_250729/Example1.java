e8738d4eb47dca546d4f84fa9dfa729103bd067b
OngTK <dhdxorud@gmail.com>
2025-07-29 11:16:37 +0900

    250729_1116

--- src/daily/day22_250729/Example1.java
+++ src/daily/day22_250729/Example1.java
@@ -9,6 +9,18 @@ package daily.day22_250729;
         2. 유니코드
             : 전 세계 공용어, Java에서 사용
 
+    1. 문자열 사용하는 방법
+        1) "문자열"                : 큰 따옴표("")안에 문자열 표현
+        2) new String("문자열")    : 생성자를 이용한 문자열 표현
+    2. 문자열 비교 방법
+        1) 문자열A == 문자열B       : 리터럴 문자만 비교 가능
+        2) 문자열A.equals(문자열B)  : 리터럴과 객체 문자열 비교 가능
+    3. 주요 메소드
+        1) 문자열A.concat(문자열B)                            : 연결 메소드
+        2) String.format("형식문자", 문자열)                  : 지정한 형식으로 문자열을 반환하는 메소드
+        3) StringBuilder builder = new StringBuilder();     : 문자열을 연결해주는 클래스
+            builder.append(문자열A);
+
 */
 
 import java.util.Arrays;
@@ -78,6 +90,57 @@ public class Example1 {
 
         StringBuilder builder2 = new StringBuilder();
         builder2.append("insert into tb(name) values (");                           // 그닥.....
+
+
+        // 5) .charAt( 인덱스 ) : 지정한 인덱스 번호의 문자 1개 반환 메소드
+        char gender = "012345-1234567".charAt(7);
+        System.out.println(gender);                         // 1
+        // [활용] scan.next().charAt(x) : console에서 받은 값의 index x 번째 1개를 추출
+
+        // 6) . length() : 문자 수 반환 메소드
+        System.out.println("012345-1234567".length());      // 14
+        // [활용] 반복문
+
+        // 7) .replace( As-is문자열, To-be문자열 )
+        //      : 문자열 추출 > As-is문자열 삭제, To-be문자열 삽입 > 새로운 문자열 생성 메소드
+        //      : 새로 생성하는 메소드이므로 주소값이 바뀜
+        String str12 = "자바프로그래밍".replace("자바","Java");
+        System.out.println(str12);
+        // [활용] HTML 줄바꿈 <br> >> Java '\n'
+        String htmlData = "유재석<br>안녕하세요.";
+        htmlData.replaceAll("<br>","\n");
+
+        // 8) .subString( 시작인덱스, (끝인덱스) ); : 시작 인덱스부터 마지막 인덱스 전까지 추출
+        String str13 = "012345-1234567".substring(0,6);
+        System.out.println(str13);      // 012345
+
+        String str14 = "012345-1234567".substring(7);  // 7번 index부터 마지막 까지 출력
+        System.out.println(str14);      // 1234567
+        // [활용] 차량번호 조회 : 214가7531 / 차량번호 뒷차리 = 차량번호.subString( 차량번호.length()-4 )
+
+        // 9) .split("구분문자") : 문자열 내 구분문자를 기준으로 문자열을 자른 후 배열로 반환
+        String[] str15 = "012345-1234567".split("-");
+        System.out.println(str15[0]);           // 012345
+        System.out.println(str15[1]);           // 1234567
+        // [활용] CSV 형식 다루기
+        //      쉼표(,)로 속성을 구분하고 \n으로 줄구분을 함
+        //      날짜(y-m-d) / 주소 (시, 군, 구, 동)
+
+        // 10) .indexOf("찾을 문자열") : 문자열 내 "찾을 문자열"이 존재하면 찾은 인덱스를 반환, 없으면 -1을 반환
+        int findIndex = "자바 프로그래밍 언어".indexOf("프로");
+        System.out.println(findIndex);      // 3
+        // [활용] 검색
+
+        // 11) .contains("찾을 문자열") : 문자열 내 "찾을 문자열"이 존재하면 true, 없으면 false
+        boolean findBool = "자바 프로그래밍 언어".contains("프로");
+        System.out.println(findBool);       //true
+        // [활용] 검색
+
+        // 12) .getBytes() : 지정 문자열을 byte[] 타입으로 반환하는 메소드
+        byte[] str16 = "JAva program".getBytes();
+        System.out.println(Arrays.toString(str16));     // 아스키 코드 [74, 65, 118, 97, 32, 112, 114, 111, 103, 114, 97, 109]
+        // [활용] 파일처리, 네트워크 통신 etc
+
 
     } // main end
 } // class end
---
+++ src/daily/day22_250729/Example2.java
@@ -0,0 +1,4 @@
+package daily.day22_250729;
+
+public class Example2 {
+}
