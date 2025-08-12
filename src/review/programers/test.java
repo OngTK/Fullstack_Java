9e779210b5cd63db4db417d15e419211a0deb262
OngTK <dhdxorud@gmail.com>
2025-08-12 10:02:35 +0900

    250812_1002

---
+++ src/review/programers/test.java
@@ -0,0 +1,18 @@
+package review.programers;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int answer = 0;
+
+        for(int i=0; i< number ; i++){
+            answer += number % 100;
+            number /= 100;
+        }
+
+        System.out.println(answer);
+    }
+}
