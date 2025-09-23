ca238ef82b678ea8d31a6d69062ea4e39745f04d
OngTK <dhdxorud@gmail.com>
2025-09-23 17:49:03 +0900

    백준

--- src/review/programers/Main.java
+++ src/review/programers/Main.java
@@ -6,11 +6,30 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("         ,r'\"7");
-        System.out.println("r`-_   ,'  ,/");
-        System.out.println(" \\. \". L_r'");
-        System.out.println("   `~\\/");
-        System.out.println("      |");
-        System.out.println("      |");
+        Scanner scan = new Scanner(System.in);
+
+        char[][] a = new char[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            String s = scan.next();
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == ' ') {
+                    a[i][j] = '!';
+                } else {
+                    a[i][j] = s.charAt(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(a[j][i] ==  '\0' ){
+                    continue;
+                } else {
+                    System.out.print(a[j][i]);
+                }
+
+            }
+        }
     }
 }
